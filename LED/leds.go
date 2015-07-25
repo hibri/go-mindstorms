04e255cf85dedbcbe4abb5c87341da183d64868d
Lev Berman <ldmberman@gmail.com>
2015-07-25 23:43:14 +0300

    Update the LED package after the LED API was changed.

--- LED/leds.go
+++ LED/leds.go
@@ -30,7 +30,11 @@ func findFilename(color Color, position Position) string {
 		log.Fatal("Amber colors must be decomposed into green and red")
 	}
 
-	filename := fmt.Sprintf("/sys/class/leds/ev3:%s:%s", string(color), string(position))
+	index := 1
+	if color == Red {
+		index = 0
+	}
+	filename := fmt.Sprintf("/sys/class/leds/ev3-%s%d:%s:ev3dev", string(position), index, string(color))
 
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		log.Fatal("Cannot find the LED interface")
