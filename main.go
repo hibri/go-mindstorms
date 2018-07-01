1385dc2d6652a88c388ac8fd2a9665f52fd8f8f2
Hibri Marzook <hibri@hibri.net>
2018-07-01 20:53:34 +0100

    detect color

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/hibri/GoEV3/Motor"
@@ -10,11 +11,26 @@ import (
 
 func main() {
 	sensor := Sensors.FindInfraredSensor(Sensors.InPort2)
+	colorSensor := Sensors.FindColorSensor(Sensors.InPort1)
+
 	fmt.Printf("Current speed Motor A %d", Motor.CurrentSpeed(Motor.OutPortA))
-	Motor.Run(Motor.OutPortA, 40)
-	Motor.Run(Motor.OutPortB, 40)
+	fmt.Printf("Current Sensor reading %d", sensor.ReadProximity())
+
+	Motor.Run(Motor.OutPortA, 150)
+	Motor.Run(Motor.OutPortB, 150)
 
 	for {
+
+		color := colorSensor.ReadColor()
+		log.Print(color)
+		if color == Sensors.Red {
+			Motor.Stop(Motor.OutPortA)
+			Motor.Stop(Motor.OutPortB)
+		}
+		if color == Sensors.White {
+			Motor.Run(Motor.OutPortA, -100)
+			Motor.Run(Motor.OutPortB, -100)
+		}
 		value := sensor.ReadProximity()
 
 		if value < 50 {
