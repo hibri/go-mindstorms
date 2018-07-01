9040402e4a3d0c0aae149bdd24e480a2fc7e2a54
Hibri Marzook <hibri@hibri.net>
2018-07-01 20:30:49 +0100

    remove regulation mode as it has been deprecated

--- Motor/motors.go
+++ Motor/motors.go
@@ -2,10 +2,11 @@
 package Motor
 
 import (
-	"github.com/ldmberman/GoEV3/utilities"
 	"log"
 	"os"
 	"path"
+
+	"github.com/hibri/GoEV3/utilities"
 )
 
 // Constants for output ports.
@@ -22,15 +23,14 @@ const (
 const (
 	rootMotorPath = "/sys/class/tacho-motor"
 	// File descriptors for getting/setting parameters
-	portFD           = "address"
-	regulationModeFD = "speed_regulation"
-	speedGetterFD    = "speed"
-	speedSetterFD    = "speed_sp"
-	powerGetterFD    = "duty_cycle"
-	powerSetterFD    = "duty_cycle_sp"
-	runFD            = "command"
-	stopModeFD       = "stop_command"
-	positionFD       = "position"
+	portFD        = "address"
+	speedGetterFD = "speed"
+	speedSetterFD = "speed_sp"
+	powerGetterFD = "duty_cycle"
+	powerSetterFD = "duty_cycle_sp"
+	runFD         = "command"
+	stopModeFD    = "stop_command"
+	positionFD    = "position"
 )
 
 func findFolder(port OutPort) string {
@@ -52,38 +52,19 @@ func findFolder(port OutPort) string {
 		}
 	}
 
-	log.Fatal("No motor is connected to port ", port )
+	log.Fatal("No motor is connected to port ", port)
 	return ""
 }
 
-// Runs the motor at the given port.
-// The meaning of `speed` parameter depends on whether the regulation mode is turned on or off.
+// Runs the motor at the given port, untill stop is called
 //
-// When the regulation mode is off (by default) `speed` ranges from -100 to 100 and
-// it's absolute value indicates the percent of motor's power usage. It can be roughly interpreted as
-// a motor speed, but deepending on the environment, the actual speed of the motor
-// may be lower than the target speed.
-//
-// When the regulation mode is on (has to be enabled by EnableRegulationMode function) the motor
-// driver attempts to keep the motor speed at the `speed` value you've specified
-// which ranges from about -1000 to 1000. The actual range depends on the type of the motor - see ev3dev docs.
-//
-// Negative values indicate reverse motion regardless of the regulation mode.
+// Negative values indicate reverse motion
 func Run(port OutPort, speed int16) {
 	folder := findFolder(port)
-	regulationMode := utilities.ReadStringValue(folder, regulationModeFD)
 
-	switch regulationMode {
-	case "on":
-		utilities.WriteIntValue(folder, speedSetterFD, int64(speed))
-		utilities.WriteStringValue(folder, runFD, "run-forever")
-	case "off":
-		if speed > 100 || speed < -100 {
-			log.Fatal("The speed must be in range [-100, 100]")
-		}
-		utilities.WriteIntValue(folder, powerSetterFD, int64(speed))
-		utilities.WriteStringValue(folder, runFD, "run-forever")
-	}
+	utilities.WriteIntValue(folder, speedSetterFD, int64(speed))
+	utilities.WriteStringValue(folder, runFD, "run-forever")
+
 }
 
 // Stops the motor at the given port.
@@ -99,17 +80,6 @@ func CurrentSpeed(port OutPort) int16 {
 // Reads the operating power of the motor at the given port.
 func CurrentPower(port OutPort) int16 {
 	return utilities.ReadInt16Value(findFolder(port), powerGetterFD)
-}
-
-// Enables regulation mode, causing the motor at the given port to compensate
-// for any resistance and maintain its target speed.
-func EnableRegulationMode(port OutPort) {
-	utilities.WriteStringValue(findFolder(port), regulationModeFD, "on")
-}
-
-// Disables regulation mode. Regulation mode is off by default.
-func DisableRegulationMode(port OutPort) {
-	utilities.WriteStringValue(findFolder(port), regulationModeFD, "off")
 }
 
 // Enables brake mode, causing the motor at the given port to brake to stops.
